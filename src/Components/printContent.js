e809b55daf2b3f251175e0743981350b1c14b50b
LianYiiii <903572705@qq.com>
2022-04-01 00:14:24 +0800

    prepare for print comments

--- src/Components/printContent.js
+++ src/Components/printContent.js
@@ -1,61 +1,99 @@
 import axios from 'axios';
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import PerContent from './PerContent';
 
-const renderInfo = [];
 
+const RequestAjax = (() => {
+    const renderInfo = [];
+    const [data, setData] = useState({ renderInfo: [] });
 
-export default class PrintContent extends Component {
-    // state = {
-    //     "talk":
-    //         [{
-    //             "author": "laowang",
-    //             "createTime": "2022-12-26 13:14",
-    //             "file-id": "01",
-    //             "content": "hello,i'm laowang",
-
-    //         }, {
-    //             "author": "laolian",
-    //             "createTime": "2022-12-28 05:21",
-    //             "file-id": "02",
-    //             "content": "hello,i'm laolian",
-    //         }]
-    // }
-
-    
-    render() {
-        // console.log(fileInfo.talk.length);
-        // console.log(this.state.talk[0]);
-        axios({
+    useEffect(() => {
+        const getData = async () => {
+            const waitAxios = await axios({
                 headers: {
                     'Authorization': localStorage.getItem('token')
                 },
                 method: 'get',
                 url: 'http://192.168.43.118:8000/api/newsfeeds/'
-        }).then(res => {
-            // console.log(res.data);
-            for (let item = 0; item < res.data.newsfeeds.length;item++) {
-                // console.log(res.data.newsfeeds[item]);
-                let items = res.data.newsfeeds[item];
-                let author = items.dynamic.user? items.dynamic.user.username:'匿名';
-                let content = items.dynamic.content;
-                let createTime = items.dynamic.created_at;
-                renderInfo.push(
-                    <>
-                        <div>123</div>
-                    <PerContent key={item} /></>
-                );
-            }    
-            console.log(renderInfo);
-        }).catch(err => {
-            console.log(err);
-        })
-        const waitAsy = await 
-          
-        return (
-            <>
-                {renderInfo}
-            </>
-        )
-    }
+                // url: 'http://10.0.1.119:8000/api/newsfeeds/'
+            }).then(res => {
+                    console.log(res.data);
+                    console.log(res.data.newsfeeds);
+                    for (let item = 0; item < res.data.newsfeeds.length; item++) {
+                        // console.log(res.data.newsfeeds[item]);
+                        console.log(res);
+                        let items = res.data.newsfeeds[item];
+                        let author = items.dynamic.user ? items.dynamic.user.username : '匿名';
+                        let content = items.dynamic.content;
+                        let createTime = items.dynamic.created_at;
+                        // console.log(author,content,createTime);
+                        renderInfo.push(
+                            <>
+                                <div>123</div>
+                                <PerContent author={author} content={content} createTime={createTime} /></>
+                        );
+                    }
+                }).catch(err => {
+                    console.log(err);
+                })
+                setData(waitAxios.data)
+
+        }
+        getData();
+    }, []);
+
+    console.log(renderInfo);
+
+    return (
+        <>
+
+        </>
+    )
+})
+
+export default function PrintContent() {
+    return (
+        <><RequestAjax /></>
+    )
 }
+
+
+
+
+    // 异步未完成
+    // render() {
+    //     axios({
+    //         headers: {
+    //             'Authorization': localStorage.getItem('token')
+    //         },
+    //         method: 'get',
+    //         // url: 'http://192.168.72.1:8000/api/newsfeeds/'
+    //         url: 'http://10.0.1.119:8000/api/newsfeeds/'
+    //     }).then(res => {
+    //         // console.log(res.data.newsfeeds);
+    //         for (let item = 0; item < res.data.newsfeeds.length; item++) {
+    //             // console.log(res.data.newsfeeds[item]);
+    //             let items = res.data.newsfeeds[item];
+    //             let author = items.dynamic.user ? items.dynamic.user.username : '匿名';
+    //             let content = items.dynamic.content;
+    //             let createTime = items.dynamic.created_at;
+    //             renderInfo.push(
+    //                 <>
+    //                     <div>123</div>
+    //                     <PerContent key={item} /></>
+    //             );
+    //         }
+    //         // console.log(renderInfo);
+    //     }).catch(err => {
+    //         console.log(err);
+    //     })
+
+
+
+    // return(
+    //         <>
+    // { renderInfo }
+    //         </>
+    //     )
+
+// }
